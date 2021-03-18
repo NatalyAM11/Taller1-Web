cc6670984937dda743a1e3f47dd19de75c3e2596
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2021-03-18 11:59:33 -0500

    modal

--- js/index.js
+++ js/index.js
@@ -5,11 +5,13 @@ const cstormiWeather=document.querySelector ('.stormiWeather');
 const cstormiWorld=document.querySelector ('.stormiWorld');
 const hamburgerBtn=document.querySelector ('.nav__hamburger');
 const interactionLi=document.querySelectorAll ('.interaction__li');
+const modal=document.querySelector ('.modal');
+const modalBtn=document.querySelector ('.modal__btn');
 
 //variables interaction
 
 const imgEye=document.querySelector ('.interaction__eye');
-const nameOption=document.getElementById ('nameOption');
+const nameOption=document.querySelector('.interaction__name');
 
 
 
@@ -23,81 +25,7 @@ hamburgerBtn.addEventListener('click', ()=>{
 });
 
 
-
-//////interaction section
-
-/*function handleInteractionClick(){
-    imgEye.src='./img/interaction1.png'; 
-    
-    cperfectStorm.src='./img/option1Selected.png';
-
-    cbutterflyBae.src='./img/option2.png';
-    cstormiWeather.src='./img/option3.png';
-    cstormiWorld.src='./img/option4.png';
-
-    //Change the product's name
-    nameOption.innerHTML='PERFECT STORM';
-}
-
-
-cperfectStorm.addEventListener('click', handleInteractionClick);
-
-
-
-
-
-
-
-//second option
-cbutterflyBae.addEventListener('click', ()=>{
-     //change the image sample
-    imgEye.src='./img/interaction2.png';
-
-    cbutterflyBae.src='./img/option2Selected.png';
-
-    cperfectStorm.src='./img/option1.png';
-    cstormiWeather.src='./img/option3.png';
-    cstormiWorld.src='./img/option4.png';
-
-    //Change the product's name
-    nameOption.innerHTML='BUTTERFLY BAE';
-  
-});
-
-
-//third option
-cstormiWeather.addEventListener('click', ()=>{
-    //change the image sample
-   imgEye.src='./img/interaction3.png';
-
-   cstormiWeather.src='./img/option3Selected.png';
-
-   cperfectStorm.src='./img/option1.png';
-   cbutterflyBae.src='./img/option2.png';
-   cstormiWorld.src='./img/option4.png';
-
-    //Change the product's name
-    nameOption.innerHTML='STORMI WEATHER'; 
-});
-
-//fourth option
-cstormiWorld.addEventListener('click', ()=>{
-    //change the image sample
-   imgEye.src='./img/interaction4.png';
-
-   cstormiWorld.src='./img/option4Selected.png';
-
-   cperfectStorm.src='./img/option1.png';
-   cbutterflyBae.src='./img/option2.png';
-   cstormiWeather.src='./img/option3.png';
-
-    //Change the product's name
-    nameOption.innerHTML='STORMI WORLD';    
-  
-})*/
-
-
-//interaction section
+/////interaction section
 function handleForEach (element, i){
 
     function handleInteractionClick (){
@@ -175,3 +103,26 @@ function handleForEach (element, i){
 
 
 interactionLi.forEach(handleForEach);
+
+
+
+
+
+
+/////Modal
+
+function handleModalDisappear(){
+    modal.style.display='none';
+}
+
+function closeModal(){
+    modal.style.opacity=0;
+    //modal.style.zIndex=1;
+
+    //body
+    document.body.style.overflow= 'visible';
+
+    setTimeout(handleModalDisappear,300);
+}
+
+modalBtn.addEventListener('click', closeModal);
