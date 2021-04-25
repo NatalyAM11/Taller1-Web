b37396d4b7d9fef9d398074a3611c00652489136
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2021-04-25 15:58:01 -0500

    Form progress

--- js/productForm.js
+++ js/productForm.js
@@ -13,15 +13,57 @@ firebase.initializeApp(firebaseConfig);
 
 //database
 const db = firebase.firestore();
-
+const storage= firebase.storage();
 
 
 const productForm= document.querySelector('.productForm');
+const productFormLoading= document.querySelector('.productForm__loader');
+const productFormError= document.querySelector('.productForm__error');
+const productFormReminder= document.querySelector('.productForm__reminder');
+const reminderText= document.querySelector('.productForm__reminder--text');
+const productFormImg= document.querySelector('.productForm__bigImg');
+const littleImg= document.querySelector('.productForm__sideImg');
+const i= document.querySelector('.productForm__i');
+
+const imgFiles= [];
+
+let popularityProduct;
+
+//image product
+productForm.image.addEventListener('change', function (){
+    
+    const file= productForm.image.files[0];
+
+    if(!file){
+        return;
+    }
+
+    i.classList.add('hidden');
+
+    const reader= new FileReader();
+
+    reader.onload= function(event){
+        //big image
+        productFormImg.setAttribute('src', event.target.result);
+
+        //side images
+        const miniImg=document.createElement('img');
+        miniImg.classList.add('productForm__littleImg');
+        miniImg.setAttribute('src', event.target.result);
+        littleImg.appendChild(miniImg);
+    }
+
+    reader.readAsDataURL(file);
+    imgFiles.push(file);
+});
+
 
 //send the information
 productForm.addEventListener('submit', function(event){
     //delete the event by default
     event.preventDefault();
+
+    popularityProduct= Math.floor(Math.random() * 6);    
 
     //product
     const product={
@@ -32,21 +74,129 @@ productForm.addEventListener('submit', function(event){
         content: productForm.content.value,
         tone: productForm.tone.value,
         collection: productForm.collection.value,
-        description:productForm.description.value
+        description:productForm.description.value,
+        popularity: popularityProduct
     }
 
+    //Errors if the client doesn't fill all the fields
+
+    let error= "";
+
+    if (!product.name){
+        error+= "Don't forget the name of the product </br>";
+    }
+    if (!product.p){
+        error+= 'The product is required </br>';
+    }
     if (!product.type){
-
+        error+= 'You must select the type of the product </br>';
+    }
+    if (!product.price){
+        error+= 'The price of the product is required </br>';
+    }
+    if (!product.content){
+        error+= "Don't forget the content of the product </br>";
+    }
+    if (!product.description){
+        error+= "Don't forget the description of the product </br>";
     }
 
-    console.log(product);
+    if(error){
+        productFormReminder.innerHTML=error;
+        productFormReminder.classList.remove('hidden');
+        return;
+    }else{
+        productFormReminder.classList.add('hidden');
+    }
 
-    //send the product in firebase
+    productFormLoading.classList.remove('hidden');
+    productFormError.classList.add('hidden');
+
+    const genericCatch= function (error){
+        productFormError.innerHTML="Something went wrong, try again please";
+        productFormLoading.classList.add('hidden');
+        productFormError.classList.remove('hidden');
+    }
+
+    //send the product to firebase
     db.collection('products').add(product).then(
         function(docRef){
-            //aqui pongo el modal o lo que pase si se subio el producto correctamente
-            
-            console.log('document added', docRef.id);
-        }
-    );
+             //aqui pongo el modal o lo que pase si se subio el producto correctamente
+
+            //array promises upload
+            const uploadPromises=[];
+            //array with url promises
+            const downloadUrlPromises=[];
+
+            //////image reference
+            imgFiles.forEach(function(file){
+                //create the root reference
+                var storageRef=firebase.storage().ref();
+
+                //root
+                var productImgRef= storageRef.child(`products/${docRef.id}/${file.name}`);
+
+                //add the images in the array
+                uploadPromises.push(productImgRef.put(file));
+
+            });
+
+            //get the download URL of the image
+            Promise.all(uploadPromises).then(function(snapshots){
+                snapshots.forEach((snapshot)=>{
+
+                    //add the ulr in the array
+                    downloadUrlPromises.push(snapshot.ref.getDownloadURL());
+                });
+
+                //wait till all the promises are done
+                Promise.all(downloadUrlPromises).then(function (downloadUrls){
+                    console.log(downloadUrls);
+
+                    //create an array for the url of the img
+                    const images= [];
+                    
+                    downloadUrls.forEach(function(url, index){
+                        images.push(
+                            {
+                                url: url,
+                                ref: snapshots[index].ref.fullPath
+                            });
+                    });
+                    
+                    //add the new information of the images to the product
+                    db.collection('products').doc(docRef.id).update({
+                            images: images
+                    }).then(function(){
+                        //all the product has been upload                      
+                        console.log('document added', docRef.id);
+                        productFormLoading.classList.add('hidden');
+                    })
+                    .catch(genericCatch);
+                })
+                .catch(genericCatch);
+            })
+            .catch(genericCatch);
+        })
+        .catch(genericCatch);
 });
+
+/*.then((downloadURL)=>{
+
+                        //loading
+                        productFormLoading.classList.remove('hidden');
+
+                        product.imageUrl= downloadURL;
+                        product.imageRef= snapshot.ref.fullPath;
+
+                    
+                    });*/
+
+
+
+
+/*.then((snapshot)=>{
+
+                    //wait to get the download URL of the image
+                    snapshot.ref.getDownloadURL();
+                });*/
