f10f7c405280eb50cca6a097fb63c664a7044827
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2021-05-23 19:46:13 -0500

    prueba error

--- js/index.js
+++ js/index.js
@@ -140,23 +140,3 @@ function handleForEach (element, i){
 interactionLi.forEach(handleForEach);
 
 
-
-
-/////////////////Modal
-//turn the display of the modal in none
-/*function handleModalDisappear(){
-    modal.style.display='none';
-}
-
-function handleCloseModal(){
-    //transition
-    modal.style.opacity=0;
-
-    //make the body visible then the modal disappear
-    document.body.style.overflow= 'visible';
-
-    //wait a while for the transition to be seen and then return to the modal none
-    setTimeout(handleModalDisappear,300);
-}
-
-modalBtn.addEventListener('click', handleCloseModal);*/
